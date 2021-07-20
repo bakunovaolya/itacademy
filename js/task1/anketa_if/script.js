3a2e46258ad599b450a051b2103df99ac8b91d45
Olga Bakunova <olya.bakunova@gmail.com>
2021-07-21 00:46:41 +0300

    done js task1

---
+++ js/task1/anketa_if/script.js
@@ -0,0 +1,27 @@
+const GENDER_MAN = 'мужской';
+const GENDER_WOMEN = 'женский';
+ 
+ let surname = prompt('Введите вашу фамилию');
+ let name = prompt('Веведите ваше имя');
+ let patronymic = prompt('Введите ваше отчество');
+ let age = parseInt(prompt('Веведите ваш возраст'));
+ let gender = confirm('ваш пол - мужской?');
+ let gendername, pension;
+
+if (gender) {
+    gendername = GENDER_MAN;
+} else {
+    gendername = GENDER_WOMEN;
+}
+if ((gendername === GENDER_MAN && age >= 63) || (gendername === GENDER_WOMEN && age >= 60)){
+    pension = 'да';
+} else {
+    pension = 'нет';
+}
+
+alert (' ваше фио: '+surname+' '+name+ ' '+patronymic+
+        '\nваш возраст в годах: ' + age+
+        '\nваш возраст в днях: ' + (age*365)+
+        '\nчерез 5 лет вам будет: ' + (age+5) + 
+        '\nваш пол: ' + gendername +
+        '\nвы на пенсии: ' + pension);
